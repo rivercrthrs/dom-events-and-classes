bc2244fbd0c878261cf279a734056d9ec67c85f3
rivercrthrs <rivercrud@gmail.com>
2016-11-03 15:41:08 -0700

    added make invisibble

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', makeInvisible)
   }
 }
 
@@ -28,6 +29,10 @@ function makeBlue (evt) {
   updateCounts ()
 }
 // CREATE FUNCTION hide HERE
+function makeInvisible (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts ()
+}
 
 function updateCounts () {
   var totals = {
